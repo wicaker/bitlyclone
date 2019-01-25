1647592c564f4c98c5652c56727eb9e7e4e4affc
wicak <wicaksanaigede@gmail.com>
2019-01-25 12:30:46 +0800

    Updates

---
+++ precache-manifest.527207279d6c7d236c8711b50fa58db4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04fc6fb90a6198fb3173",
+    "url": "/bitlyclone/static/js/runtime~main.04fc6fb9.js"
+  },
+  {
+    "revision": "97630a3a81f59240d770",
+    "url": "/bitlyclone/static/js/main.97630a3a.chunk.js"
+  },
+  {
+    "revision": "a61e788606e4091ba650",
+    "url": "/bitlyclone/static/js/1.a61e7886.chunk.js"
+  },
+  {
+    "revision": "97630a3a81f59240d770",
+    "url": "/bitlyclone/static/css/main.4dd2f46d.chunk.css"
+  },
+  {
+    "revision": "9347ee72d99f0d74814aba1d7ef50c02",
+    "url": "/bitlyclone/index.html"
+  }
+];
